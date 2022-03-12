11956b7ee694ea141b78570fb050814a46808125
j7breuer <jmbreuer@vt.edu>
2022-03-12 09:34:03 -0500

    updates to module

--- app/huggingface_sentiment.py
+++ app/huggingface_sentiment.py
@@ -1,4 +1,10 @@
 
+from collections import defaultdict
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
 
 class TweetSentimentClassifier:
     def __init__(self):
@@ -13,12 +19,35 @@ class TweetSentimentClassifier:
             "sp": "spanish",
             "pt": "portuguese"
         }
+        self.models = {
+            "model": AutoModelForSequenceClassification.from_pretrained(self.model_name),
+            "tokenizer": AutoTokenizer.from_pretrained(self.model_name),
+            "config": AutoConfig.from_pretrained(self.model_name)
+        }
 
-    def load_tokenizer(self):
-        return AutoTokenizer.from_pretrained(self.model_name)
+    def preprocess(text):
+        new_text = []
+        for t in text.split(" "):
+            t = '@user' if t.startswith('@') and len(t) > 1 else t
+            t = 'http' if t.startswith('http') else t
+            new_text.append(t)
+        return " ".join(new_text)
 
-    def load_config(self):
-        return AutoConfig.from_pretrained(self.model_name)
+    def sentiment_single(self, text):
+        # Simple preprocessing step
+        text = self.preprocess(text)
 
-    def load_model(self):
-        return AutoModelForSequenceClassification.from_pretrained(self.model_name)
+        # Run tokenizer/model
+        encoded_input = self.tokenizer(text, return_tensors = 'pt')
+        output = self.model(**encoded_input)
+
+        # Gather scores
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+        
+        # Generate output dict
+        oupt_dict = {
+            self.config.id2label[0]: scores[0],
+            self.config.id2label[1]: scores[1],
+            self.config.id2label[2]: scores[2]
+        }
